579b28983b0aa897d37797068bd3ebfb00b9a2da
AntoineCoignac <acoignac35@gmail.com>
2023-10-30 22:24:58 +0100

    dotenv config

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -4,6 +4,8 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const { validationResult } = require('express-validator');
 const nodemailer = require('nodemailer');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const noReply = "noreply@like.cr";
 
