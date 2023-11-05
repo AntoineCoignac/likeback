21277b0a1cf50c9701f572e849bf1b7b66853cc1
AntoineCoignac <acoignac35@gmail.com>
2023-11-05 18:02:41 +0100

    new mail config with gmail

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -10,17 +10,13 @@ dotenv.config();
 const noReply = "noreply@like.cr";
 
 const transporter = nodemailer.createTransport({
-  host : "smtp-mail.outlook.com",
-  service: 'hotmail',
-  secureConnection: false,
-  port : 587,
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true,
   auth: {
-    user : noReply,
-    pass: process.env.MAIL
-  },
-  tls: {
-    ciphers:'SSLv3',
-    rejectUnauthorized: false,
+      user: 'noreplike@gmail.com',
+      pass: process.env.MAIL
+
   }
 })
 
@@ -90,20 +86,20 @@ const login = async (req, res, next) => {
     const { password, ...info } = user._doc;
 
     const mailOptions = {
-      from: noReply,
+      from: 'noreplike@gmail.com',
       to: info.email,
       subject: "Connexion au compte",
       html : `<table style="width: 100%; background-color: #f5f5f5; padding: 20px;">
                 <tr>
                     <td>
-                        <img src="https://like.cr/img/likelogo.png" alt="Like logo." style="margin: 0 auto 20px;" width="60" height="38">
+                        <img src="https://like.cr/img/likelogo.png" alt="Like logo." style="margin: 0 auto 20px;" width="60" height="32">
                     </td>
                 </tr>
                 <tr>
                     <td style="background-color: #ffffff; padding: 20px; border: 1px solid #A7A7A7;">
-                        <p>Bonjour ${info.name},</p>
+                        <p>Bonjour <span style="font-weight : 500;">${info.name}</span>,</p>
                         <p>Quelqu'un vient de se connecter à votre compte. Si ce n'est pas vous, veuillez modifier votre mot de passe.</p>
-                        <a href="https://app.like.cr" style="background-color: #D91A3D; color: #ffffff; text-decoration: none; padding: 10px 20px; margin-top: 14px; border-radius: 3px; display: block;">Modifier le mot de passe</a>
+                        <a href="https://app.like.cr" style="background-color: #D91A3D; color: #ffffff; text-align: center; text-decoration: none; padding: 10px 20px; margin-top: 14px; font-weight: 500px; border-radius: 3px; display: block;">Modifier le mot de passe</a>
                     </td>
                 </tr>
             </table>`
